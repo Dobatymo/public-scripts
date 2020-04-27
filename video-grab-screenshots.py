53d42fa4a1bd03593ac225dd33684d91d5f3a137
Dobatymo <Dobatymo@users.noreply.github.com>
2020-04-27 13:43:33 +0800

    improved video-video-grab-screenshots

--- video-grab-screenshots.py
+++ video-grab-screenshots.py
@@ -1,8 +1,10 @@
 from __future__ import absolute_import, division, print_function, unicode_literals
 
+import logging
+
 from genutility.compat.pathlib import Path
 from genutility.videofile import grab_pic
-from genutility.args import suffix, existing_path
+from genutility.args import suffix, existing_path, between
 
 if __name__ == "__main__":
 
@@ -13,19 +15,24 @@ if __name__ == "__main__":
 	parser.add_argument("outpath", type=Path) # , type=is_dir
 	parser.add_argument("--inext", type=suffix, default=".mp4")
 	parser.add_argument("--outext", type=suffix, default=".jpg")
-	parser.add_argument("--skip", action="store_true")
-	parser.add_argument("--backend", choices=("av", "cv"), default="av")
+	parser.add_argument("--backend", choices=("av", "cv"), default="cv")
+	parser.add_argument("--overwrite", action="store_true", help="Overwrite existing files")
+	parser.add_argument("--pos", nargs="*", default=[0.5], type=between(0, 1), help="List of of file position ratios")
 	args = parser.parse_args()
+
+	if len(args.pos) == 1:
+		pos = args.pos[0]
 
 	if args.inpath.is_file():
 		assert not args.outpath.is_dir()
-		grab_pic(args.inpath, args.outpath, backend=args.backend)
+		grab_pic(args.inpath, args.outpath, pos, args.overwrite, args.backend)
 
 	else:
 		for path_in in args.inpath.rglob("*" + args.inext):
 			path_out = args.outpath / path_in.relative_to(args.inpath)
 			path_out = path_out.with_suffix(args.outext)
-			print(path_in, "->", path_out)
-			if args.skip and path_out.exists(): # skip
-				continue
-			grab_pic(path_in, path_out, backend=args.backend)
+			logging.info("Processing %s", path_in)
+			try:
+				grab_pic(path_in, path_out, pos, args.overwrite, args.backend)
+			except FileExistsError:
+				logging.info("Skipping existing file %s", path_in)
